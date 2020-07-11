3a444a7bef749b024f182e698e89f06a43f5f76e
Abhishek Biswas <64304355+codebyabhishek772@users.noreply.github.com>
2020-07-11 20:38:17 +0530

    Add files via upload

---
+++ Arithmatic_operation.f95
@@ -0,0 +1,31 @@
+program Arithmatic_operation
+    
+    INTEGER :: var1 = 10
+    
+    INTEGER :: var2 = 20
+    
+    sum_of_var = var1 + var2 
+    
+    subtract_of_var = var2 - var1
+    
+    division_of_var = var2 / var1
+    
+    multipication_of_var = var1 * var2
+    
+    modulo_of_var = mod(var1, var2)
+    
+    power_of_var = (var1 ** var2)
+    
+    print *, sum_of_var
+    
+    print *, subtract_of_var
+    
+    print *, division_of_var
+    
+    print *, multipication_of_var
+    
+    print *, modulo_of_var
+    
+    print *, power_of_var
+    
+end program Arithmatic_operation
---
+++ taking_input_from_user.f95
@@ -0,0 +1,15 @@
+program Take_input_from_user
+
+CHARACTER*20 :: name 
+
+CHARACTER (len = 20) :: f_name , l_name
+
+read *, f_name, l_name
+
+print *, "Hello", " " ,f_name, " ",l_name
+
+read *, name 
+
+print *, "Hello", " " ,name 
+
+end program Take_input_from_user
