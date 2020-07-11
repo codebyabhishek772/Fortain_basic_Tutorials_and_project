c6be3fc363a4d8a4df529cd264c634553f5b4169
Abhishek Biswas <64304355+codebyabhishek772@users.noreply.github.com>
2020-07-11 13:40:17 +0530

    Add files via upload

---
+++ intro_and_hello_world_project.f95
@@ -0,0 +1,9 @@
+program hello
+   Print *, "Hello World!"
+   print *, "This is my first Fortrain program"
+   print *, "Fortrain has 3 major version which are Fortarin 77 , Fortain 90 and Mordern Fortrain"
+   print *, "Fortrain 77 ----> Its the version of fortrain relesed in 1977 "
+   print *, "Fortrain 90 ---->  Its the version of fortarin relesed in 1990-99"
+   print *, "Mordern Fortrain -----> Its the version of Fortrain relesed after mid 2000s"
+   print *, "Fortain means Formula Translation and use first in NASA and its a High level langauge"
+end program Hello
